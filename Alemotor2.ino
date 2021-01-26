77de0394009af28302a6c0ae715b5573fd12888a
Marcos <arcemontecino@gmail.com>
2021-01-26 16:52:20 -0300

    add button encoder

--- Alemotor2.ino
+++ Alemotor2.ino
@@ -2,76 +2,66 @@
 #include "Arduino.h"
 #include <RotaryEncoder.h>
 
-
 LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
-
 RotaryEncoder encoder(13, 27);
 #define ROTARYSTEPS 1
 int lastPos = -1;
+int pulsador = 32;
+
 
 int rotaryValor=1;
 bool modificar=false;
 int check=0;
 
-//Variables de distancia y velocidad
 int velocidad=0;
 int distancia=0;
 
-
-
 void setup()
 {
-  pinMode (32, INPUT_PULLUP); //boton de un esp32
+  pinMode (pulsador, INPUT_PULLUP); //boton de un esp32
   Serial.begin(115200);
   lcd.init();
   lcd.clear();
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("MotorControl  v1");
-  //delay(2000);
+  delay(2000);
   lcd.clear();
 }
-
-
 void loop()
 {
   rotary(1,4);
-  	
-
-  
-  
-														 
-
-  
- 
-  
 }
 void rotary(int ROTARYMIN,int ROTARYMAX){
-  
-
+  int presionado = 0;
   encoder.tick();
-
   int newPos= encoder.getPosition() * ROTARYSTEPS;
-
   if (newPos < ROTARYMIN) {
     encoder.setPosition(ROTARYMIN / ROTARYSTEPS);
     newPos = ROTARYMIN;
-
   } else if (newPos > ROTARYMAX) {
     encoder.setPosition(ROTARYMAX / ROTARYSTEPS);
     newPos = ROTARYMAX;
-  } // if
-
+  } 
   if (lastPos != newPos) {
-  Serial.print(newPos);
-  Serial.println();
+  Serial.println(newPos);
   menuDisplay(newPos);
-  //rotaryValor=newPos;  
   lastPos = newPos;
+  }
+  
+  //////////////////Encoder Boton////////////
+  if (digitalRead(pulsador) == LOW) 
+  {
+    presionado = 1; 
+    delay(50);
+  }
+  if (digitalRead(pulsador) == HIGH && presionado == 1)
+  {
+    Serial.println("chao");
+    presionado = 0;
   }
 }
 void menuDisplay(int mode){
-
    switch(mode){
     case 1:
         lcd.clear();
@@ -90,7 +80,6 @@ void menuDisplay(int mode){
         lcd.print("Velocidad");
         if (modificar==true){
           modificar=2;
-          //velocidad = encoderValue;
           lcd.setCursor(0,1);
           lcd.print(velocidad);
         }
@@ -99,14 +88,11 @@ void menuDisplay(int mode){
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Distancia"); 
-        
         if (modificar==true){
           modificar=3;
-          //distancia = encoderValue;
           lcd.setCursor(0,1);
           lcd.print(distancia);
         }
-        
         break;
     case 4:
         lcd.clear();
