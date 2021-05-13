7ecf4a1eb7cd84057e42e34eaed4a1eb98b7e3b3
Marcos <arcemontecino@gmail.com>
2021-05-13 13:12:30 -0400

    web files

---
+++ data/test.js
@@ -0,0 +1,48 @@
+
+var data = ""
+function Move() {
+    var VelocidadX = document.getElementById("VelX").value;
+    var DistanciaX = document.getElementById("DisX").value;
+    var VelocidadY = document.getElementById("VelY").value;
+    var DistanciaY = document.getElementById("DisY").value;
+    var timer = document.getElementById("Tiempo").value;
+    Offtime(timer);
+    data = `${VelocidadX},${DistanciaX},${VelocidadY},${DistanciaY}`
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/Move?value=" + data, true);
+    xhr.send();
+}
+function MoveX() {
+    var VelocidadX = document.getElementById("VelX").value;
+    var DistanciaX = document.getElementById("DisX").value;
+    var timer = document.getElementById("Tiempo").value;
+    Offtime(timer);
+    data = `${VelocidadX},${DistanciaX}`
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/MoveX?value=" + data, true);
+    xhr.send();
+}
+function MoveY() {
+    var VelocidadY = document.getElementById("VelY").value;
+    var DistanciaY = document.getElementById("DisY").value;
+    var timer = document.getElementById("Tiempo").value;
+    Offtime(timer);
+    data = `${VelocidadY},${DistanciaY}`
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/MoveY?value=" + data, true);
+    xhr.send();
+}
+function Stop() {
+    data = "Stop"
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/Stop?value=" + data, true);
+    xhr.send();
+}
+
+function Offtime(timedata) {
+    if (timedata >= 1) {
+        setTimeout(function () {
+            Stop();
+        }, timedata * 1000);
+    }
+}
