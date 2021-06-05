ab1e95bc1dc2b780ea0d9530c14260da2d6b9d7e
Marcos <arcemontecino@gmail.com>
2021-06-05 19:21:05 -0400

    modo avanzado creado

---
+++ data/avanzado.js
@@ -0,0 +1,15 @@
+function Mode1(){
+
+    document.getElementById('Lvalue1').innerHTML = 'M1 Value1';
+    document.getElementById('Lvalue2').innerHTML = 'M1 Value2';
+}
+function Mode2(){
+
+    document.getElementById('Lvalue1').innerHTML = 'M2 Value1';
+    document.getElementById('Lvalue2').innerHTML = 'M2 Value2';
+}
+function Mode3(){
+
+    document.getElementById('Lvalue1').innerHTML = 'M3 Value1';
+    document.getElementById('Lvalue2').innerHTML = 'M3 Value2';
+}
