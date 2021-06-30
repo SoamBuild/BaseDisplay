d7ffb6b462fc0be0e145174b3865c0917f316720
Marcos <arcemontecino@gmail.com>
2021-06-30 10:15:57 -0400

    lista de frecuencias

--- data/avanzado.js
+++ data/avanzado.js
@@ -1,15 +1,25 @@
 function Mode1(){
 
-    document.getElementById('Lvalue1').innerHTML = 'M1 Value1';
+    document.getElementById('Lvalue1').innerHTML = '';
     document.getElementById('Lvalue2').innerHTML = 'M1 Value2';
 }
 function Mode2(){
 
-    document.getElementById('Lvalue1').innerHTML = 'M2 Value1';
-    document.getElementById('Lvalue2').innerHTML = 'M2 Value2';
+    document.getElementById('Lvalue1').innerHTML = 'Distancia mm';
+    document.getElementById('Lvalue2').innerHTML = 'Tiempo Seg';
 }
 function Mode3(){
 
+   // var e = document.getElementById("ddlViewBy");   
+   // var strUser = e.value;  
+   // console.log(strUser);
     document.getElementById('Lvalue1').innerHTML = 'M3 Value1';
     document.getElementById('Lvalue2').innerHTML = 'M3 Value2';
+}
+
+function Move(){
+    var e = document.getElementById("Frecuencias");   
+    var strUser = e.value;  
+    console.log(strUser);
+
 }
