82151348c8de0ffa96e6a948d4b49a912ff308d0
Marcos <arcemontecino@gmail.com>
2021-06-30 11:56:07 -0400

    esconder elementos segun modo

--- data/avanzado.js
+++ data/avanzado.js
@@ -1,6 +1,6 @@
 function Mode1() {
-    document.getElementsByClassName("Select").style.display="none";
-    //document.getElementsByClassName("moveit").style.display="none";
+    document.getElementsByClassName("Valuetomove")[0].style.display="display: inline-block";
+   
     document.getElementById('Lvalue1').innerHTML = '';
     document.getElementById('Lvalue2').innerHTML = 'M1 Value2';
 }
@@ -10,6 +10,7 @@ function Mode2() {
     document.getElementById('Lvalue2').innerHTML = 'Tiempo Seg';
 }
 function Mode3() {
+    document.getElementsByClassName("Valuetomove")[0].style.display="none";
     document.getElementById("Frecuencias").style.display="inline";
     // var e = document.getElementById("ddlViewBy");   
     // var strUser = e.value;  
