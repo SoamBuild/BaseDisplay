97e91a80fea211eed916b9909ac4e9380b20dbce
Marcos <arcemontecino@gmail.com>
2021-06-30 22:38:25 -0400

    modo avanzado

--- data/avanzado.js
+++ data/avanzado.js
@@ -1,27 +1,16 @@
 function Mode1() {
-
-
     showinputdata();
-    document.getElementById('Lvalue1').innerHTML = '';
-    document.getElementById('Lvalue2').innerHTML = 'M1 Value2';
+    document.getElementById('Lvalue1').innerHTML = 'Velocidad mm/s';
+    document.getElementById('Lvalue2').innerHTML = 'tiempo Seg';
 }
 function Mode2() {
-
+    showinputdata();
     document.getElementById('Lvalue1').innerHTML = 'Distancia mm';
     document.getElementById('Lvalue2').innerHTML = 'Tiempo Seg';
 }
 function Mode3() {
     showselectdata();
-    //  document.getElementsByClassName("Valuetomove")[0].style.visibility = "hidden";
-    //document.getElementsByClassName("Valuetomove")[0].style.display="visibility: hidden"; 
-    // document.getElementsByClassName("Valuetomove")[0].style.display = "none";
-    // alert("text")
-    //document.getElementsByClassName("Select")[0].style.display = "inline";
-    //document.getElementById("Frecuencias").style.display = "inline";
-    // var e = document.getElementById("ddlViewBy");   
-    // var strUser = e.value;  
-    // console.log(strUser);
-
+  
 }
 
 function Move() {
