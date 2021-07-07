b9caa24bf03504d6adf0a67a03d550b2eec30dcd
Marcos <arcemontecino@gmail.com>
2021-07-07 12:51:02 -0400

    add move general function

--- data/avanzado.js
+++ data/avanzado.js
@@ -1,9 +1,15 @@
 let seleccion_hz;
 let seleccion_usr;
+
 let dis_mat;
 let vel_mat;
+
 let data1_usr;
 let data2_usr;
+
+let final_vel;
+let final_dis;
+
 let mode;
 
 function Mode1() {
@@ -11,7 +17,7 @@ function Mode1() {
     showinputdata();
     document.getElementById('Lvalue1').innerHTML = 'Velocidad mm/s';
     document.getElementById('Lvalue2').innerHTML = 'Tiempo Seg';
-   }
+}
 function Mode2() {
     mode = 2;
     showinputdata();
@@ -26,6 +32,21 @@ function Mode3() {
 
 function Move() {
     getdata(mode);
+    /*
+    if (mode == 1) {
+        data = `${VelocidadX},${DistanciaX},${VelocidadY},${DistanciaY}`
+    }
+    if (mode == 2) {
+        data = `${VelocidadX},${DistanciaX},${VelocidadY},${DistanciaY}`
+    }
+*/
+    data = `${final_vel},${final_dis},${final_vel},${final_dis}`;
+    console.log(data);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/Move?value=" + data, true);
+    xhr.send();
+
+    getdata(mode);
 }
 function showinputdata() {
     document.getElementsByClassName("Valuetomove")[0].style.display = "inline-block";
@@ -37,7 +58,7 @@ function showselectdata() {
     document.getElementsByClassName("Select")[0].style.display = "inline";
     document.getElementById("Frecuencias").style.display = "inline";
 }
-function getdata(modedata){
+function getdata(modedata) {
     data1_usr = document.getElementById("value1").value;
     data2_usr = document.getElementById("value2").value;
 
@@ -50,15 +71,22 @@ function getdata(modedata){
     }
     if (modedata == 2) {
 
-        setVel(data1_usr,data2_usr);
+        setVel(data1_usr, data2_usr);
 
     }
 }
 function setDis(input_vel_dis, input_tim_dis) {
+    final_vel = input_vel_dis;
+
     dis_mat = input_vel_dis * input_tim_dis;
+
+    final_dis = dis_mat;
     alert(`Distancia esperada ${dis_mat}mm`);
 }
-function setVel(input_dis_vel,input_tim_vel){
-    vel_mat= input_dis_vel / input_tim_vel;
+function setVel(input_dis_vel, input_tim_vel) {
+    final_dis=input_dis_vel;
+
+    vel_mat = input_dis_vel / input_tim_vel;
+    final_vel = vel_mat;
     alert(`Velocidad esperada ${vel_mat}mm/s`);
 }
